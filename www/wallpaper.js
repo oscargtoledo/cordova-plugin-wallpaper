a373bd10d42b37cb8571eef7271ce5542d10e3ec
Òscar García Toledo <narcyz2048@gmail.com>
2023-01-02 16:34:34 +0100

    Added setImageHttpDL

--- www/wallpaper.js
+++ www/wallpaper.js
@@ -116,6 +116,108 @@ wallpaper.prototype.setImageHttp = function(url, type, callback) {
     }
 };
 
+// Function that implements downloading an image, saving it to persistent storage, and setting it as a wp
+// POC for saving files in Cordova
+wallpaper.prototype.setImageHttpDL = function (url, type, callback) {
+    var request = null;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject) {
+        try {
+            request = new ActiveXObject("Msxml2.XMLHTTP.6.0") || new ActiveXObject("Msxml2.XMLHTTP.3.0");
+        }
+        catch (e) {
+            typeof callback === 'function' && callback('XMLHttpRequest is not supported.');
+        }
+    }
+    if (request == null) {
+        typeof callback === 'function' && callback('XMLHttpRequest is not supported.');
+    }
+    else {
+        function XHRload() {
+            var array = new Uint8Array(request.response);
+            console.log(JSON.stringify(array));
+            var raw = '';
+            var chunk = 5000;
+            for (i = 0; i < array.length; i += chunk) {
+                var subArray = array.subarray(i, i + chunk);
+                raw += String.fromCharCode.apply(null, subArray);
+            }
+            var base64 = btoa(raw);
+            console.log( "Content" + base64);
+            var contentDispo = request.getResponseHeader('Content-Disposition');
+            // https://stackoverflow.com/a/23054920/
+            var fileName = contentDispo.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1].replaceAll("\"","");
+            console.log(fileName.replaceAll("\"",""));
+
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+
+                console.log('file system open: ' + fs.name);
+                fs.root.getFile(fileName, { create: true, exclusive: false }, function (fileEntry) {
+
+                    console.log("fileEntry is file?" + fileEntry.isFile.toString());
+                    writeFile(fileEntry, base64);
+
+                }, ((e) => { console.log("Error createfile"); JSON.stringify(e) }));
+
+            }, ((e) => { console.log("Error filesystem"); JSON.stringify(e) }));
+
+            function writeFile(fileEntry, dataObj) {
+                console.log(dataObj);
+                // Create a FileWriter object for our FileEntry (log.txt).
+                fileEntry.createWriter(function (fileWriter) {
+
+                    fileWriter.onwriteend = function () {
+                        console.log("Successful file write...");
+                        readFile(fileEntry);
+                    };
+
+                    fileWriter.onerror = function (e) {
+                        console.log("Failed file write: " + e.toString());
+                    };
+
+                    // If data object is not passed in,
+                    // create a new Blob instead.
+                    if (!dataObj) {
+                        dataObj = new Blob(['some file data'], { type: 'text/plain' });
+                    }
+
+                    fileWriter.write(dataObj);
+                });
+            }
+
+            function readFile(fileEntry) {
+
+                fileEntry.file(function (file) {
+                    var reader = new FileReader();
+
+                    reader.onloadend = function () {
+                        console.log("Successful file read: " + this.result);
+                        setBase64(reader.result, callback);
+                    };
+                    reader.readAsText(file);                    
+                }, ((e) => { console.log("Error createfile"); JSON.stringify(e) }));
+            }
+            
+
+        }
+
+        function XHRerror(error) {
+            typeof callback === 'function' && callback(error);
+        }
+        function XHRabort(error) {
+            typeof callback === 'function' && callback(error);
+        }
+        request.responseType = "arraybuffer";
+        request.addEventListener("load", XHRload);
+        request.addEventListener("error", XHRerror);
+        request.addEventListener("abort", XHRabort);
+        request.open("GET", url, true);
+        request.send();
+    }
+};
+
 wallpaper.install = function() {
     if (!window.plugins) {
         window.plugins = {};
